cf180feb88a37d514e0ccde0be75834cc6476025
pixelMarck <marcohdes94i@gmail.com>
2019-01-28 13:47:46 -0700

    termine tarea de billetes

--- prac-11.py
+++ prac-11.py
@@ -3,32 +3,59 @@
 
 def billetes():
 
-    cantidad = int(input("dinero > "))
+    
 
-    if (cantidad >=1000):
-        this_change = cantidad / 1000
-        this_change2 =  cantidad % 1000
-        print("billetes", int(this_change),  "residuo",this_change2)
+    try:
+        
+        cantidad = int(input("dinero > "))  
 
-        if(this_change2 >= 500 and this_change2 <= 999):
-                
+        if(cantidad > 1):
+            this_change = cantidad / 1000
+            this_change2 =  cantidad % 1000
+            print("billetes 1000 > ", int(this_change))
+
+                        
             this_change3 = this_change2 / 500
             this_change_3 = this_change2 % 500
-            print("billetes de 500 > ", int(this_change3), "residuo > ", this_change_3)
+            print("billetes de 500 > ", int(this_change3))
 
-            if(this_change_3 >=200 and this_change_3 <=499):
-                
-                this_change4 = this_change_3 / 200
-                this_change_4 = this_change_3 % 200
-                print("billetes de 200> ", int(this_change4), " ", "residuo > ", this_change_4)
-
-                if (this_change_4 >=100 and this_change_4 <=199):
-
-                    this_change5 = this_change_4 / 100
-                    this_change_5 = this_change_4 % 100
-                    print("billetes de 100 > ", int(this_change5), " ", "residuo > ", this_change_5)
+                        
+            this_change4 = this_change_3 / 200
+            this_change_4 = this_change_3 % 200
+            print("billetes de 200 > ", int(this_change4))
 
 
+            this_change5 = this_change_4 / 100
+            this_change_5 = this_change_4 % 100
+            print("billetes de 100 > ", int(this_change5))
+
+
+            this_change6 = this_change_5 / 50
+            this_change_6 = this_change_5 % 50
+            print("billetes de 50 > ", int(this_change6))
+
+            this_change7 = this_change_6 / 20
+            this_change_7 = this_change_6 % 20
+            print("billetes 20 >", int(this_change7) )
+
+            this_change8 = this_change_7 / 10
+            this_change_8 = this_change_7 % 10
+            print("monedas de 10 > ", int(this_change8))
+
+            this_change9 = this_change_8 / 5
+            this_change_9 = this_change_8 % 5
+            print("monedas de 5 pesos > ", int(this_change9))
+
+            this_change10 = this_change_9 / 1
+            this_change_10 = this_change_9 % 1
+            print("monedas de 1 peso >  ", int(this_change10))
+        
+        else:
+            print("solo se aceptan valores numeros positivos mayores que 1")
+
+
+    except:
+        return print("Solo se aceptan valores numericos positivos")
 
 
 
